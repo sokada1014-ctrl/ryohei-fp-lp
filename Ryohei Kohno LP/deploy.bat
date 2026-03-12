6846b0a5a52a308d99f0e1f1ca53cef265e6685d
sokada1014-ctrl <s.okada1014@gmail.com>
2026-03-12 14:59:19 +0900

    LP修正

---
+++ Ryohei Kohno LP/deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "C:\Users\sokad\OneDrive\デスクトップ\program\portfolio LP"
+git add index.html S__83419139.jpg
+git commit -m "Update LP"
+git push
+echo.
+echo デプロイ完了！Vercelに反映されました。
+pause
